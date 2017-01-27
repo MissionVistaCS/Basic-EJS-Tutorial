1f754eb7986733557fed19c3cdcb5f6cdf542742
Andrew Yates <andrewmacu@me.com>
2017-01-27 12:41:36 -0800

    Update app.js

--- app.js
+++ app.js
@@ -14,3 +14,7 @@ app.set('view engine', 'ejs');
 app.get('/', (request, response) => {
     
 });
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
+  var addr = server.address();
+});
